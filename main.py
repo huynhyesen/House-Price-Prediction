53024681ff493175da9dcab32f16e694ddcf4433
Yesen <huynhhoangyen1609@gmail.com>
2024-11-28 15:00:05 +0700

    UPDATE

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("data.csv")
@@ -9,7 +10,9 @@ row, col = data.shape
 x = data.drop(columns = [data.columns[col - 1]])
 y = data[data.columns[col - 1]]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1)
+train_data, test_data, train_label, test_label = train_test_split(x, y, test_size = 0.1)
 
-
-
+x.hist(bins=20, figsize=(10, 8))
+plt.tight_layout()
+plt.savefig('histogram_plot.png')
+plt.close()
